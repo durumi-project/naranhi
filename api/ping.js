4bee1177526d25fd58f6d172ca52afc47914f282
Aonosakujitsu <thkim11@yonsei.ac.kr>
2026-05-17 21:37:46 +0900

    diag: ping function

---
+++ api/ping.js
@@ -0,0 +1,5 @@
+export default async function handler(request) {
+  return new Response(JSON.stringify({ ok: true, time: Date.now() }), {
+    headers: { 'content-type': 'application/json' }
+  });
+}
