54567198d2f06fad1185b6c67f97b61f519f897f
ChanhuiSeok <chanhuicom@gmail.com>
2026-05-18 19:42:53 +0900

    fix: ping.js 수정

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,3 @@
-export default async function handler(request) {
-  return new Response(JSON.stringify({ ok: true, time: Date.now() }), {
-    headers: { 'content-type': 'application/json' }
-  });
+export default function handler(req, res) {
+  res.status(200).json({ ok: true, time: Date.now() });
 }
